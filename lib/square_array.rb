84a906680821abfc6748460599e68ea608ddd713
Matthew Sewell <mattfsewell@gmail.com>
2020-11-21 20:51:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+
 def square_array(array)
-  # your code here
+  
+  count = 0
+  while count < array.length {
+   array[count] = array[count] * array[count]
+    count += 1
+  }
+  
+    return array
+  
 end
